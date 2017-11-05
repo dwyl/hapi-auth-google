8c9ecfa4340f8eca6198de63a240a08b7bdfbee2
Yulia <yulia.tenincheva@mentormate.com>
2017-11-05 14:35:17 +0200

    Refactoring & v17 tweaks

--- example/google.server.js
+++ example/google.server.js
@@ -1,13 +1,9 @@
 'use strict';
 
-require('env2')('.env');
+require('env2')('.env'); // suggestion: replace with dotenv
+
 const assert = require('assert');
 const Hapi = require('hapi');
-
-const server = new Hapi.Server({
-  host: process.env.HOST || 'localhost',
-  port: process.env.PORT || 8000
-});
 
 const options = {
   REDIRECT_URL: '/googleauth',  // must match google app redirect URI
@@ -17,41 +13,57 @@ const options = {
   scope: 'https://www.googleapis.com/auth/plus.profile.emails.read' // profile
 };
 
-server.register([{plugin: require('../lib'), options}]).then(() => {
-
-  // Returns HTML with "Login with Google" button
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      var url = server.generate_google_oauth2_url();
-
-      var imgsrc = 'https://developers.google.com/accounts/images/sign-in-with-google.png';
-      var btn = '<a href="' + url +'"><img src="' + imgsrc + '" alt="Login With Google"></a>';
-
-      return btn;      
-    }
+async function init() {
+  const server = new Hapi.Server({
+    host: process.env.HOST || 'localhost',
+    port: process.env.PORT || 8000
   });
 
-  server.start().then(() => {
-    console.log('Hapi.js Version Running', server.version);
+  try {
+    await server.register([{plugin: require('../lib'), options}]);
 
+    // Returns HTML with "Login with Google" button
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: (request, h) => {
+        var url = server.generate_google_oauth2_url();
+
+        var imgsrc = 'https://developers.google.com/accounts/images/sign-in-with-google.png';
+        var btn = '<a href="' + url +'"><img src="' + imgsrc + '" alt="Login With Google"></a>';
+          
+        return btn;      
+      }
+    });
+
+  } catch(err) {
+    console.log(err);
+    assert(!err, "Failed to load the plugin!");
+  };
+    
+  try {
+    await server.start();
+    console.log('Hapi.js Version Running', server.version);
+     
     console.log('Registered Routes:')
     server.table().forEach((route) => console.log(`${route.method}\t${route.path}`));
-
+      
     console.log('Now Visit: http://localhost:' + server.info.port);
-  }).catch(err => {
+
+  } catch(err) {
     console.log('Error!', err);
     process.exit(1);
-  });
+  };
 
-}).catch(err => {
-  console.log(err);
-  assert(!err, "Failed to load the plugin!");
-});
+  return server;
+}
+
+if(!module.parent) {
+  init();
+}
+
+module.exports = init;
 
 // process.on('uncaughtException', function(data) {
 //   console.log(data);
 // });
-
-module.exports = server;
--- test/google.test.js
+++ test/google.test.js
@@ -6,80 +6,86 @@ const nock = require('nock');
 const dir  = __dirname.split('/')[__dirname.split('/').length-1];
 const file = dir + __filename.replace(__dirname, '') + " > ";
 
-const server = require('../example/google.server');
-console.log('Testing with Hapi Server v', server.version);
+initTests();
 
-test(file + 'Visit / root url expect to see a link', async (t) => {
-  var options = {
-    method: "GET",
-    url: "/"
-  };
-
-  try {
-    let response = await server.inject(options);
-    t.equal(response.statusCode, 200, "Server is working.");
-    await server.stop();
-  } catch(err) {
-    // console.log(err);
-  };
-  
-  t.end();
-});
-
-// test a bad code does not crash the server!
-test(file + '/googleauth?code=oauth2codehere', async (t) => {
-  var options = {
-    method: "GET",
-    url: "/googleauth?code=badcode"
-  };
-
-  try {
-    let response = await server.inject(options);
-    t.equal(response.statusCode, 400, "Server is working.");
-    t.ok(response.payload.indexOf('Bad Request') > -1, 'Got: ' + response.payload + ' (As Expected)');
+async function initTests() {
+    const server = await require('../example/google.server')();
     
-    await server.stop();
-  } catch(err) {
-    // console.log(err);
-  };
+    console.log('Testing with Hapi Server v', server.version);
 
-  t.end();
-});
 
-test(file + 'Mock /googleauth?code=oauth2codehere', async (t) => {
-  // google oauth2 token request url:
-  const fs = require('fs');
-  const nock = require('nock');
-  
-  var token_fixture = fs.readFileSync('./test/fixtures/sample-auth-token.json');
-  var scope = nock('https://accounts.google.com')
+    test(file + 'Visit / root url expect to see a link', async (t) => {
+        var options = {
+            method: "GET",
+            url: "/"
+        };
+
+        try {
+            let response = await server.inject(options);
+            t.equal(response.statusCode, 200, "Server is working.");
+            await server.stop();
+        } catch(err) {
+            // console.log(err);
+        };
+        
+        t.end();
+    });
+
+    // test a bad code does not crash the server!
+    test(file + '/googleauth?code=oauth2codehere', async (t) => {
+        var options = {
+            method: "GET",
+            url: "/googleauth?code=badcode"
+        };
+
+        try {
+            let response = await server.inject(options);
+            t.equal(response.statusCode, 400, "Server is working.");
+            t.ok(response.payload.indexOf('Bad Request') > -1, 'Got: ' + response.payload + ' (As Expected)');
+            
+            await server.stop();
+        } catch(err) {
+            // console.log(err);
+        };
+
+        t.end();
+    });
+
+    test(file + 'Mock /googleauth?code=oauth2codehere', async (t) => {
+        // google oauth2 token request url:
+        const fs = require('fs');
+        const nock = require('nock');
+        
+        var token_fixture = fs.readFileSync('./test/fixtures/sample-auth-token.json');
+        var scope = nock('https://accounts.google.com')
             .persist() // https://github.com/pgte/nock#persist
             .post('/o/oauth2/token')
             .reply(200, token_fixture);
 
-  // see: http://git.io/v4nTR for google plus api url
-  // https://www.googleapis.com/plus/v1/people/{userId}
-  var sample_profile = fs.readFileSync('./test/fixtures/sample-profile.json');
-  var scope = nock('https://www.googleapis.com')
-      .get('/plus/v1/people/me')
-      .reply(200, sample_profile);
+        // see: http://git.io/v4nTR for google plus api url
+        // https://www.googleapis.com/plus/v1/people/{userId}
+        var sample_profile = fs.readFileSync('./test/fixtures/sample-profile.json');
+        var scope = nock('https://www.googleapis.com')
+            .get('/plus/v1/people/me')
+            .reply(200, sample_profile);
 
-  var options = {
-    method: "GET",
-    url: "/googleauth?code=myrandomtoken"
-  };
+        var options = {
+            method: "GET",
+            url: "/googleauth?code=myrandomtoken"
+        };
 
-  try {
-    var expected = 'Hello Alex You Logged in Using Goolge!';
-    var response = await server.inject(options);
-    
-    t.equal(response.statusCode, 200, "/googleauth returns valid oauth2 token");
-    t.equal(response.payload, expected, "> " + expected);
+        try {
+            var expected = 'Hello Alex You Logged in Using Goolge!';
+            var response = await server.inject(options);
+            
+            t.equal(response.statusCode, 200, "/googleauth returns valid oauth2 token");
+            t.equal(response.payload, expected, "> " + expected);
 
-    await server.stop();
-    
-    t.end();
-  } catch (err) {
-    // console.log(err);
-  }
-});
+            await server.stop();
+            
+            t.end();
+        } catch (err) {
+            // console.log(err);
+        }
+    });
+}

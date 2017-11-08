4df077dda33d566710594917a0767dbdd3289590
Yulia Tenincheva <yulia.tenincheva@mentormate.com>
2017-11-08 19:14:43 +0200

    Added test to validate response due to no profile data fetched

--- test/google.test.js
+++ test/google.test.js
@@ -9,83 +9,119 @@ const file = dir + __filename.replace(__dirname, '') + " > ";
 initTests();
 
 async function initTests() {
-    const server = await require('../example/google.server')();
+  const server = await require('../example/google.server')();
+  
+  console.log('Testing with Hapi Server v', server.version);
+
+
+  test(file + 'Visit / root url expect to see a link', async (t) => {
+    var options = {
+      method: "GET",
+      url: "/"
+    };
+
+    try {
+      let response = await server.inject(options);
+      t.equal(response.statusCode, 200, "Server is working.");
+      await server.stop();
+    } catch(err) {
+      // console.log(err);
+    };
     
-    console.log('Testing with Hapi Server v', server.version);
+    t.end();
+  });
 
+  // test a bad code does not crash the server!
+  test(file + '/googleauth?code=oauth2codehere', async (t) => {
+    var options = {
+      method: "GET",
+      url: "/googleauth?code=badcode"
+    };
 
-    test(file + 'Visit / root url expect to see a link', async (t) => {
-        var options = {
-            method: "GET",
-            url: "/"
-        };
+    try {
+      let response = await server.inject(options);
+      t.equal(response.statusCode, 400, "Server is working.");
+      t.ok(response.payload.indexOf('Bad Request') > -1, 'Got: ' + response.payload + ' (As Expected)');
+      
+      await server.stop();
+    } catch(err) {
+      // console.log(err);
+    };
 
-        try {
-            let response = await server.inject(options);
-            t.equal(response.statusCode, 200, "Server is working.");
-            await server.stop();
-        } catch(err) {
-            // console.log(err);
-        };
-        
-        t.end();
-    });
+    t.end();
+  });
 
-    // test a bad code does not crash the server!
-    test(file + '/googleauth?code=oauth2codehere', async (t) => {
-        var options = {
-            method: "GET",
-            url: "/googleauth?code=badcode"
-        };
+  test(file + 'Mock /googleauth?code=oauth2codehere', async (t) => {
+    // google oauth2 token request url:
+    const fs = require('fs');
+    const nock = require('nock');
+    
+    var token_fixture = fs.readFileSync('./test/fixtures/sample-auth-token.json');
+    var scope = nock('https://accounts.google.com')
+        .persist() // https://github.com/pgte/nock#persist
+        .post('/o/oauth2/token')
+        .reply(200, token_fixture);
 
-        try {
-            let response = await server.inject(options);
-            t.equal(response.statusCode, 400, "Server is working.");
-            t.ok(response.payload.indexOf('Bad Request') > -1, 'Got: ' + response.payload + ' (As Expected)');
-            
-            await server.stop();
-        } catch(err) {
-            // console.log(err);
-        };
+    // see: http://git.io/v4nTR for google plus api url
+    // https://www.googleapis.com/plus/v1/people/{userId}
+    var sample_profile = fs.readFileSync('./test/fixtures/sample-profile.json');
+    var scope = nock('https://www.googleapis.com')
+        .get('/plus/v1/people/me')
+        .reply(200, sample_profile);
 
-        t.end();
-    });
+    var options = {
+      method: "GET",
+      url: "/googleauth?code=myrandomtoken"
+    };
 
-    test(file + 'Mock /googleauth?code=oauth2codehere', async (t) => {
-        // google oauth2 token request url:
-        const fs = require('fs');
-        const nock = require('nock');
-        
-        var token_fixture = fs.readFileSync('./test/fixtures/sample-auth-token.json');
-        var scope = nock('https://accounts.google.com')
-            .persist() // https://github.com/pgte/nock#persist
-            .post('/o/oauth2/token')
-            .reply(200, token_fixture);
+    try {
+      var expected = 'Hello Alex You Logged in Using Goolge!';
+      var response = await server.inject(options);
+      
+      t.equal(response.statusCode, 200, "/googleauth returns valid oauth2 token");
+      t.equal(response.payload, expected, "> " + expected);
 
-        // see: http://git.io/v4nTR for google plus api url
-        // https://www.googleapis.com/plus/v1/people/{userId}
-        var sample_profile = fs.readFileSync('./test/fixtures/sample-profile.json');
-        var scope = nock('https://www.googleapis.com')
-            .get('/plus/v1/people/me')
-            .reply(200, sample_profile);
+      await server.stop();
+      
+      t.end();
+    } catch (err) {
+      // console.log(err);
+    }
+  });
 
-        var options = {
-            method: "GET",
-            url: "/googleauth?code=myrandomtoken"
-        };
+  // callback handler with no google profile should return error
+  test(file + 'Mock /googleauth?code=oauth2codehere', async (t) => {
+    const fs = require('fs');
+    const nock = require('nock');
+    
+    var token_fixture = fs.readFileSync('./test/fixtures/sample-auth-token.json');
+    var scope = nock('https://accounts.google.com')
+        .persist() // https://github.com/pgte/nock#persist
+        .post('/o/oauth2/token')
+        .reply(200, token_fixture);
 
-        try {
-            var expected = 'Hello Alex You Logged in Using Goolge!';
-            var response = await server.inject(options);
-            
-            t.equal(response.statusCode, 200, "/googleauth returns valid oauth2 token");
-            t.equal(response.payload, expected, "> " + expected);
+    var sample_profile = null; // when no data returned
+    var scope = nock('https://www.googleapis.com')
+        .get('/plus/v1/people/me')
+        .reply(200, sample_profile);
 
-            await server.stop();
-            
-            t.end();
-        } catch (err) {
-            // console.log(err);
-        }
-    });
+    var options = {
+      method: "GET",
+      url: "/googleauth?code=myrandomtoken"
+    };
+
+    try {
+      var expected = 'Sorry, something went wrong, please try again.';
+      var response = await server.inject(options);
+
+      t.equal(response.statusCode, 503, "/googleauth returns 503 when there is no profile data");
+      t.equal(response.payload, expected, "> " + expected);
+
+      await server.stop();
+      
+      t.end();
+    } catch (err) {
+      // console.log(err);
+    }
+  });
 }
